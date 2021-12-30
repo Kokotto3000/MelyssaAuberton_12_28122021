69aefdaf928f6e382db52b7ee60c381ae889eb8b
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-12-30 14:10:03 +0100

    first style on sessionsDurations

--- src/components/charts/SessionsDurations.js
+++ src/components/charts/SessionsDurations.js
@@ -1,46 +1,46 @@
 import '../../styles/SessionsDurations.css';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { AreaChart, Area, Tooltip, ResponsiveContainer, XAxis } from 'recharts';
 
 function SessionsDurations() {
     const data = [
         {
-          name: 'Page A',
+          name: 'L',
           uv: 4000,
           pv: 2400,
           amt: 2400,
         },
         {
-          name: 'Page B',
+          name: 'M',
           uv: 3000,
           pv: 1398,
           amt: 2210,
         },
         {
-          name: 'Page C',
+          name: 'M',
           uv: 2000,
           pv: 9800,
           amt: 2290,
         },
         {
-          name: 'Page D',
+          name: 'J',
           uv: 2780,
           pv: 3908,
           amt: 2000,
         },
         {
-          name: 'Page E',
+          name: 'V',
           uv: 1890,
           pv: 4800,
           amt: 2181,
         },
         {
-          name: 'Page F',
+          name: 'S',
           uv: 2390,
           pv: 3800,
           amt: 2500,
         },
         {
-          name: 'Page G',
+          name: 'D',
           uv: 3490,
           pv: 4300,
           amt: 2100,
@@ -50,25 +50,25 @@ function SessionsDurations() {
     return(
         <div className='sessions'>
             <h2>Durée moyenne des sessions</h2>
-            <div className='sessions_chart'>
+            <ResponsiveContainer className='sessions_chart'>
                 <AreaChart
-                width={250}
-                height={200}
+                // width={250}
+                // height={200}
                 data={data}
                 margin={{
-                    top: 10,
-                    right: 30,
+                    top: 0,
+                    right: 0,
                     left: 0,
                     bottom: 0,
                 }}
                 >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
+                {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                <XAxis dataKey="name" tickLine={false} axisLine={false} interval={0} maxTickGap={5}/>
+                {/* <YAxis /> */}
                 <Tooltip />
-                <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                <Area type="monotone" dataKey="uv" stroke="white" fill="white" fillOpacity="0.1" />
                 </AreaChart>
-            </div>
+            </ResponsiveContainer>
         </div>
     );
 }
