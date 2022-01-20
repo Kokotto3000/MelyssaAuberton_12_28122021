a4a7192232f93a66d4b7258915e3a7abbcbd6feb
Mélyssa Auberton <melyssa.auberton@gmail.com>
2022-01-20 10:36:53 +0100

    logo name change with props in infosCards

--- src/components/InfosCard.js
+++ src/components/InfosCard.js
@@ -1,9 +1,10 @@
 import '../styles/InfosCard.css';
 
 function InfosCard(props) {
+    console.log(props)
     return(
         <div className="infos">
-            <img className="aside_logo" src={ props.icon } alt="logo Yoga" />
+            <img className="aside_logo" src={ props.icon } alt={`logo ${props.type}`} />
             <p><span>{ props.amount }</span><br />{ props.type }</p>
         </div>
     );
